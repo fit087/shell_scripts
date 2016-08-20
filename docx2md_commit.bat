07408a917fe2e578385fa55c46806106a03f3fd9
Adolfo Correa <fitomillennium@hotmail.com>
2016-08-20 15:44:17 -0300

    Create docx2md_commit.bat

---
+++ docx2md_commit.bat
@@ -0,0 +1,5 @@
+pandoc -s Dissertação_MscAC.docx -t markdown -o Dissertação_MscAC.md
+copy /Y Dissertação_MscAC.md C:\Users\adolfo.LAMCSO0\Documents\msc_word\
+git add Dissertação_MscAC.md
+git commit
+git push
