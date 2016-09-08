dea21842dcf691cb5692e4fc5ff775460da8b82b
Adolfo Correa <fitomillennium@hotmail.com>
2016-09-08 01:15:28 -0300

    Variable file name

--- docx2md_commit.bat
+++ docx2md_commit.bat
@@ -2,14 +2,24 @@ REM http://stackoverflow.com/questions/3618863/batch-script-latin-characters
 REM use codification UTF-8 (without BOM)
 REM on notepad++ go to formatar and CodificaÃ§Ã£o em UTF-8 (sem BOM)
 REM remember use :wq to write and quit.
+REM for copy change format to ANSI, copy and after change again to UTF-8 (sem BOM)
 chcp 1252
 REM chcp 65001
-pandoc -s Dissertação_MscAC.docx -t markdown -o Dissertação_MscAC.md
-copy /Y Dissertação_MscAC.md C:\Users\adolfo.LAMCSO0\Documents\msc_word\
-cd C:\Users\adolfo.LAMCSO0\Documents\msc_word\
-git status
+REM pause
+REM set nome=Dissertação_adolfo_Rev12_fromCarl.
+
+REM never give speces in an assigment setence.
+
+set nome=Dissertação_adolfo_Rev12_fromCarl.
+pandoc -s %nome%docx -t markdown -o %nome%md
 pause
-git add Dissertação_MscAC.md
-git commit
-git push
-pause
+
+REM copy /Y %nome%md C:\Users\adolfo.LAMCSO0\Documents\msc_word\
+REM cd C:\Users\adolfo.LAMCSO0\Documents\msc_word\
+REM git status
+REM pause
+REM git add %name%md
+REM git commit
+REM git push
+REM pause
+
