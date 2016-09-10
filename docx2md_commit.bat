fdcdc4503e63056783ad1af09a4643a13fd02834
Adolfo Correa <fitomillennium@hotmail.com>
2016-09-10 15:36:18 -0300

    file with the same name
    This script pass as  a parameter the name of the filescript without extension to a command
    Then %~n0 is the name of the script in this case docx2md_commit
    at the line 24:  "pandoc -s %nome%.docx" is equal to pandoc -s docx2md_commit.docx

--- docx2md_commit.bat
+++ docx2md_commit.bat
@@ -1,25 +1,35 @@
+@echo off
 REM http://stackoverflow.com/questions/3618863/batch-script-latin-characters
 REM use codification UTF-8 (without BOM)
-REM on notepad++ go to formatar and CodificaÃ§Ã£o em UTF-8 (sem BOM)
+REM on notepad++ go to formatar and Codificação em UTF-8 (sem BOM)
 REM remember use :wq to write and quit.
 REM for copy change format to ANSI, copy and after change again to UTF-8 (sem BOM)
+
 chcp 1252
+
+REM echo %~n0
+
+REM pause
+
 REM chcp 65001
 REM pause
-REM set nome=Dissertação_adolfo_Rev12_fromCarl.
+REM set nome=Disserta褯_adolfo_Rev12_fromCarl.
 
 REM never give speces in an assigment setence.
 
-set nome=Dissertação_adolfo_Rev12_fromCarl.
-pandoc -s %nome%docx -t markdown -o %nome%md
+REM set nome=Disserta褯_adolfo_Rev12_fromCarl.
+
+set nome=%~n0
+@echo on
+pandoc -s %nome%.docx -t markdown -o %nome%.md
 pause
+@echo off
 
-REM copy /Y %nome%md C:\Users\adolfo.LAMCSO0\Documents\msc_word\
-REM cd C:\Users\adolfo.LAMCSO0\Documents\msc_word\
-REM git status
-REM pause
-REM git add %name%md
-REM git commit
-REM git push
-REM pause
-
+ copy /Y %nome%.md C:\Users\adolfo.LAMCSO0\Documents\msc_word\
+ cd C:\Users\adolfo.LAMCSO0\Documents\msc_word\
+ git status
+ pause
+ git add %name%.md
+ git commit
+ git push
+ pause
