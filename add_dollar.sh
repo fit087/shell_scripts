0696685be86f4fce13bb45cc1442cd7d161e2465
Correa.Adolfo <fitomillennium@hotmail.com>
2016-11-21 00:04:43 -0200

    Passing Command Line Arguments

--- add_dollar.sh
+++ add_dollar.sh
@@ -1,1 +1,5 @@
-sed 's/^/$ /' bashcode_001.sh > bashcode_001.txt
+#!/bin/bash
+# Add Sigil Symbol $ at the begin of each line 
+# sed 's/^/$ /' bashcode_001.sh > bashcode_001.txt
+# $ ./add_dollar.sh bashcode_001.sh
+sed 's/^/$ /' "$1" > "${1%.*}".txt
