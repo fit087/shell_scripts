c9204f596427ef845e059ae312eabef8c0142b94
Adolfo Correa <fitomillennium@hotmail.com>
2018-01-03 04:40:21 +0000

    loop for in bash

--- mount_bind.sh
+++ mount_bind.sh
@@ -1,8 +1,17 @@
-mount --bind /dev /mnt/hdd/dev
-mount --bind /dev/pts /mnt/hdd/dev/pts
-mount --bind /run /mnt/hdd/run
-mount --bind /sys /mnt/hdd/sys
-mount --bind /etc /mnt/hdd/etc
-mount --bind /usr /mnt/hdd/usr
-mount --bind /bin /mnt/hdd/bin
-mount --bind /proc /mnt/hdd/proc
+#!/bin/bash
+
+set -x
+
+# mount --bind /dev /mnt/hdd/dev
+# mount --bind /dev/pts /mnt/hdd/dev/pts
+# mount --bind /run /mnt/hdd/run
+# mount --bind /sys /mnt/hdd/sys
+# mount --bind /etc /mnt/hdd/etc
+# mount --bind /usr /mnt/hdd/usr
+# mount --bind /bin /mnt/hdd/bin
+# mount --bind /proc /mnt/hdd/proc
+
+for folders in /dev /dev/pts /run /sys /etc /usr /bin /proc
+do
+ mount --bind $folders /mnt/hdd$folders
+done
