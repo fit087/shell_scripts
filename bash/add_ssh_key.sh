bb477df2328177dc00ae75b794bd3a31705c2a69
Adolfo Correa <adolfo.correa@lamcso.coppe.ufrj.br>
2018-09-09 20:45:47 -0300

    add_ssh_key working

--- bash/add_ssh_key.sh
+++ bash/add_ssh_key.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
 
-eval $(ssh-agent -s)
-# eval `ssh-agent`
+# eval $(ssh-agent -s)
+eval `ssh-agent`
 
-ssh-add ~/.ssh/hcer2018_rsa
+# ssh-add ~/.ssh/hcer2018_rsa
+ssh-add /home/adolfo/.ssh/hcer2018_rsa
 
+# This command did this script work
+bash -i
+# bash -i        If the -i option is present, the shell is interactive.
+
+#this_sheebang_can't_be_used!/usr/bin/env bash
+#_don`tuse!/bin/bash
