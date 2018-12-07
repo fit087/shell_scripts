c2e4f077612bcc6885892e4a0c5ee1798f088276
Adolfo Correa <adolfo.correa@lamcso.coppe.ufrj.br>
2018-12-07 04:01:09 -0200

    update reinstall firefox script

--- bash/update_firefox.sh
+++ bash/update_firefox.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-path=~/Downloads/
-file=firefox-63.0.tar.bz2
+
+# path=~/Downloads/
+# file=firefox-63.0.tar.bz2
+# path=''
+path=`dirname "$1"`/
+file=`basename "$1"`
+
+# echo $path$file
+
 sudo cp $path$file /opt/ &&\
 # sudo rm $path$file &&\
 cd /opt &&\
